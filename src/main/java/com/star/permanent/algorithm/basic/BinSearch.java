d6fa59ab722179df5350833e29fc263011bb2631
suheng.cloud <suheng.cloud@shandagames.com>
2019-08-20 20:53:46 +0800

    traditional binSearch commit

--- src/main/java/com/star/permanent/algorithm/basic/BinSearch.java
+++ src/main/java/com/star/permanent/algorithm/basic/BinSearch.java
@@ -1,4 +1,28 @@
 package com.star.permanent.algorithm.basic;
 
 public class BinSearch {
+
+    private static final int[] seq = {1, 1, 2, 3, 7, 12, 14, 20, 20, 20, 20, 27, 28, 36, 44, 46, 52};
+
+    public static void main(String[] args) {
+        int i = binSearchRecur(seq, 0, seq.length - 1, 20, true);
+        System.out.println(i);
+    }
+
+    private static int binSearchRecur(int[] a, int low, int high, int t, boolean findLeftMost) {
+        if(low == high) {
+            if (a[low] == t) {
+                return low;
+            } else return -1;
+        }
+        int mid = low + ((high - low) >> 1);
+        int v = a[mid];
+        if (v == t) {
+            return findLeftMost ? binSearchRecur(a, low, mid,t, findLeftMost) : mid;
+        } else if (v < t) {
+            return binSearchRecur(a, mid + 1, high, t, findLeftMost);
+        } else {
+            return binSearchRecur(a, low, mid,t, findLeftMost);
+        }
+    }
 }
---
+++ src/main/java/com/star/permanent/algorithm/util/SeqUtil.java
@@ -0,0 +1,22 @@
+package com.star.permanent.algorithm.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SeqUtil {
+    public static void main(String[] args) {
+        genRandNums(0, 10, 15);
+    }
+
+//    [low, high]
+    private static void genRandNums(int low, int high, int cnt) {
+        Random random = new Random();
+        int minus = high - low + 1;
+        List<Integer> arr = new ArrayList<Integer>(cnt);
+        for (int i = 0;i<cnt;++i) {
+            arr.add(random.nextInt(minus) + low);
+        }
+        System.out.println(arr);
+    }
+}
