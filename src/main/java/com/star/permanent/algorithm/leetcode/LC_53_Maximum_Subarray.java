4c931abd309fe39595b1287b3417fd0a143eb5e7
suheng.cloud <suheng.cloud@shandagames.com>
2019-09-18 19:16:01 +0800

    最大子序和 两种 O(n) 算法

--- src/main/java/com/star/permanent/algorithm/leetcode/LC_53_Maximum_Subarray.java
+++ src/main/java/com/star/permanent/algorithm/leetcode/LC_53_Maximum_Subarray.java
@@ -15,42 +15,64 @@ package com.star.permanent.algorithm.leetcode;
 public class LC_53_Maximum_Subarray {
     static int[] a = {-2,1,-3,4,-1,2,1,-5,4};
     static int[] b = {4,-1,2,1};
+    static int[] c = {-11,-4,2,-5};
     public static void main(String[] args) {
-        int[] rs = edge(b);
-        for(int i = 0;i<rs.length;++i) {
+//        int[] rs = maxSubArray2(a);
+        /*for(int i = 0;i<rs.length;++i) {
             System.out.println(rs[i]);
-        }
-    }
+        }*/
+        System.out.println(maxSubArray(c));
 
+    }
 
     /*
-     * P: Array[int] = int[n]
-     * declare s=0,e=-1,curr in[0,n),delta=0,deltaM=0  [s,e] is the sub section in which sum value deltaM(candidate) is max in p
-     * p[curr] > 0 || finish => if delta>0 => delta+=P[curr],deltaM+=P[curr],update e=curr
-     *                          if delta<=0 => record(s,e,deltaM) as candidate, delta=deltaM=P[curr],s=e=curr
-     * p[curr] <=0 => delta += P[curr]
-     * */
-    public static int maxSubArray(int[] nums) {
-
-        return 0;
+    * 分治方法
+    * */
+    public static int divideConquer(int[] nums) {
+        return -1;
     }
-    private static int[] edge(int[] nums) {
+
+    /*
+    * 使用动态规划的解
+    * 寻找合并特征: 连续加和经过成员indexX时，无论经过多少次，总是仅保留最大值即可
+    * 针对pSum[indexX], 考察递推公式： pSum[indexX] = Max(pSum[indexX-1] + nums[indexX], nums[indexX])
+    * 时间复杂度 O(n) 空间复杂度O(n)
+    * */
+    public static int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int[] pSum = new int[len];
+        pSum[0] = nums[0];
+        for(int i=1;i< len;++i) {
+            pSum[i] = Math.max(pSum[i-1] + nums[i], nums[i]);
+        }
+        int max = Integer.MIN_VALUE;
+        for (int o=0;o<len;++o) {
+            if (pSum[o] > max) {
+                max = pSum[o];
+            }
+        }
+        return max;
+    }
+
+    /*
+     * 带最大子串始末位置的解
+     * 空间复杂度 O(1) 时间复杂度O(n)
+     * declare s=0,e=-1,curr in[0,n),delta=0,deltaM=0  [s,e] is the sub section in which sum value deltaM(candidate) is max in p
+     * */
+    private static int[] maxSubArray2(int[] nums) {
         int len = nums.length;
         if(len == 0) return new int[] {-1,-1};
         int[] rs = new int[2];
         int deltaS = 0, deltaE = 0;
-        int maxSum = 0;
+        int maxSum = nums[0];
         int deltaM = nums[0];
-        int delta = nums[0];
         for(int curr=1;curr<len;++curr) {
-            delta += nums[curr];
             if (nums[curr] >0 && nums[curr-1] <= 0) {  //  -  -> + 跳变
-                if (delta-nums[curr] > 0) {
-                    deltaM += nums[curr];
+                if (deltaM > 0) {
                     deltaE = curr;
                 } else {  // 丢弃 从 curr 重新开始
                     deltaS = deltaE = curr;
-                    delta = deltaM = nums[curr];
+                    deltaM = 0;
                 }
             } else if(nums[curr] <= 0 && nums[curr-1] > 0) {  // + -> - 跳变 记录可能的最大值
                 if(deltaM > maxSum) {
@@ -59,18 +81,28 @@ public class LC_53_Maximum_Subarray {
                     rs[0] = deltaS;
                 }
             } else if (nums[curr] > 0) {
-                deltaM += nums[curr];
                 deltaE = curr;
             } else if (nums[curr] <= 0) {
-              // do nothing
+                // do nothing
             }
+            deltaM += nums[curr];
         }
-        if(deltaM > maxSum) {
+        if(deltaM > maxSum) {  // 额外做一次 满足以连续正数结束状态
             maxSum = deltaM;
             rs[1] = deltaE;
             rs[0] = deltaS;
         }
-        System.out.println(maxSum);
+//        针对全部为负数场景 只要发现 maxSum < 0 需要返回min num
+        if (maxSum <0) {
+            for(int i=0;i<len;++i) {
+                if (nums[i] > maxSum) {
+                    maxSum = nums[i];
+                    rs[1] = i;
+                    rs[0] = i;
+                }
+            }
+        }
+        System.out.println(String.format("maxSum is %s",maxSum));
         return rs;
     }
 }
